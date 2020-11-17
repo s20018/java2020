041a3d2a792b149608f316b6a73e3beaa07da994
s20018 <s20018@std.it-college.ac.jp>
2020-11-18 03:23:41 +0900

    11/18

---
+++ Ex1.java
@@ -0,0 +1,112 @@
+package junit;
+
+class Ex1{
+	/**
+	 *
+	 * @param n
+	 * @return
+	 * 	n > 0 => "その値は正です"
+	 * 	n <= 0 => "その値は０か負です"
+	 */
+	public String ex1_0(int n){
+
+		return "dummy";
+
+	}
+
+	public int ex1_1(int n){
+
+		return -1;
+
+	}
+
+	public String ex1_2(int a, int b){
+
+		return "dummy";
+	}
+
+	public String ex1_3(int a, int b){
+
+		return "dummy";
+
+	}
+
+	public String ex1_4(int a){
+
+		return "dummy";
+
+	}
+
+	public String ex1_5(int a){
+
+		return "dummy";
+	}
+
+	public String ex1_6(int a){
+
+		if (a < 0)
+			return "正でない整数値です";
+		else if (a % 3)
+			return "その値を3で割った余りは1です";
+		else if (a % 3)
+			return "その値を3で割った余りは2です";
+		else
+		return "正でない整数値です";
+	}
+
+	public String ex1_7(int a){
+		return "dummy";
+	}
+
+	public double ex1_8(double a, double b){
+		return a > b ? a : b;
+		//aとbを比較して大きいほうの値を返す
+	}
+
+	public int ex1_9(int a, int b){
+		if int sa = a - b;
+		if (sa < 0)
+		return sa * -1;
+		else
+			return sa:
+		//２つの引数の間の差を求める
+	}
+
+	public String ex1_10(int a, int b){
+		int sa = a b;
+		if (sa < 0)
+            sa *= -1;
+
+            sa = 10)
+		return  "それらの差は10以下です";
+		else
+			return "それらの差は11以上です";
+
+	}
+
+	public int ex1_11(int a, int b, int c){
+		return;
+	}
+
+	public int ex1_12(int a, int b, int c){
+		if (a < b)
+			if (a < c)
+				return b;
+			else
+				return c;
+		else if (b < c)
+			if (a < c)
+				return a;
+			else
+				return c;
+		else
+			if (a < b)
+				return a;
+			else
+				return b:
+	}
+f
+	public String ex1_13(int a){
+
+	}
+}r
